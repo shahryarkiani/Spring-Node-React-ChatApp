1c68627538fd726b1314858979eb0d44c58ecf89
Shahryar K <shahryarkiani04@gmail.com>
2023-01-18 14:15:59 -0500

    Create Dockerfile

--- ChatBackend/src/test/java/com/shahryarkiani/chatbackend/ChatBackendApplicationTests.java
+++ ChatBackend/src/test/java/com/shahryarkiani/chatbackend/ChatBackendApplicationTests.java
@@ -1,13 +1,9 @@
 package com.shahryarkiani.chatbackend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ChatBackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
