ff1882947de024f4be2ca33e4a42fc8090a4e66d
Shahryar K <shahryarkiani04@gmail.com>
2023-02-03 20:31:16 -0500

    Clean up comments

--- ChatBackend/src/main/java/com/shahryarkiani/chatbackend/Messaging/MessageRepository.java
+++ ChatBackend/src/main/java/com/shahryarkiani/chatbackend/Messaging/MessageRepository.java
@@ -10,6 +10,4 @@ public interface MessageRepository extends MongoRepository<Message, String> {
     @Query("{ $or : [ {$and:[{to: ?0}, {from : ?1}]}, {$and: [{to: ?1}, {from:?0}]}] }")
     List<Message> getMessagesBetweenUseres(String user1, String user2);
 
-
-    //{ $or : [ {$and:[{to: 'bob'}, {from : 'joe'}]}, {$and: [{to: 'joe'}, {from:'bob'}]}] }
 }
