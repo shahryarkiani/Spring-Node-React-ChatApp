2fa3e6d00faa04644e7b4d428798c052305f4739
Shahryar K <shahryarkiani04@gmail.com>
2023-02-03 20:30:57 -0500

    Add query to get friend data by user id

--- ChatBackend/src/main/java/com/shahryarkiani/chatbackend/User/UserRepository.java
+++ ChatBackend/src/main/java/com/shahryarkiani/chatbackend/User/UserRepository.java
@@ -1,15 +1,23 @@
 package com.shahryarkiani.chatbackend.User;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
+    record friendData (Set<String> friends, Set<String> incomingRequests, Set<String> outgoingRequests) {}
+
+    @Query("{ _id: ?0  }, { friends: 1, incomingRequests : 1, outgoingRequests : 1, _id : 0 }")
+    Optional<friendData> getFriendsById(String id);
+
     Optional<User> findUserByUsername(String username);
 
     boolean existsByUsername(String username);
+
 
 }
